a1817b49d02dc5b0fd7067dd434e15e19a594433
Patrik Simek <patrik@patriksimek.cz>
2021-04-05 22:11:32 +0200

    Fix tests

--- test/vm.js
+++ test/vm.js
@@ -888,29 +888,31 @@ describe('VM', () => {
 		`), /e is not a function/);
 	});
 
-	it('Dynamic import attack', (done) => {
-		process.once('unhandledRejection', (reason) => {
-			assert.strictEqual(reason.message, 'process is not defined');
-			done();
+	if (NODE_VERSION >= 10) {
+		it('Dynamic import attack', (done) => {
+			process.once('unhandledRejection', (reason) => {
+				assert.strictEqual(reason.message, 'process is not defined');
+				done();
+			});
+
+			const vm2 = new VM();
+
+			vm2.run(`
+				(async () => {
+					try {
+						await import('oops!');
+					} catch (ex) {
+						// ex is an instance of NodeError which is not proxied;
+						const process = ex.constructor.constructor('return process')();
+						const require = process.mainModule.require;
+						const child_process = require('child_process');
+						const output = child_process.execSync('id');
+						process.stdout.write(output);
+					}
+				})();
+			`);
 		});
-
-		const vm2 = new VM();
-
-		vm2.run(`
-			(async () => {
-				try {
-					await import('oops!');
-				} catch (ex) {
-					// ex is an instance of NodeError which is not proxied;
-					const process = ex.constructor.constructor('return process')();
-					const require = process.mainModule.require;
-					const child_process = require('child_process');
-					const output = child_process.execSync('id');
-					process.stdout.write(output);
-				}
-			})();
-		`);
-	});
+	};
 
 	after(() => {
 		vm = null;

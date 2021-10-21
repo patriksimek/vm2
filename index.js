e42abb82e20ade0d0e3082f55981f8d8f6a57084
Matt Didon <matt@onlx.co.uk>
2021-10-21 12:52:59 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
 if (parseInt(process.versions.node.split('.')[0]) < 6) throw new Error('vm2 requires Node.js version 6 or newer.');
 
+/**
+ * Adds import helper for packaging
+ */
+try { require('./lib/contextify') } catch(_) {}
+try { require('./lib/fixasync') } catch(_) {}
+try { require('./lib/sandbox') } catch(_) {}
+
 module.exports = require('./lib/main');
